1b2e7e3e50bac11b4732dac6fb31f73e6be21e31
sayantikabanik <sayantikabanik122@gmail.com>
2022-06-21 10:57:47 +0530

    nebari CDS dashboard doc updated

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -36,7 +36,8 @@ module.exports = {
         slug: "category/tutorials",
         keywords: ["tutorials"],
       },
-      items: ["tutorials/overview"],
+      items: ["tutorials/overview",
+              "tutorials/creating-cds-dashboard"],
     },
     {
       type: "category",
