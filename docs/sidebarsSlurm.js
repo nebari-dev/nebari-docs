5208a0312869dae05d7b00555c8b1ea48f7ebd8b
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2024-06-13 05:28:53 +0530

    :wrench: Restructure into multi-instance
    Signed-off-by: Pavithra Eswaramoorthy <pavithraes@outlook.com>

---
+++ docs/sidebarsSlurm.js
@@ -0,0 +1,23 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+
+module.exports = {
+  sidebar: [
+      {
+          label: "Overview",
+          type: "doc",
+          id: "overview",
+      },
+    ],
+  }
