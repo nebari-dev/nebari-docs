876472bdda175d1e8c1bf298f50dfe83685df397
Pavithra Eswaramoorthy <pavithraes@outlook.com>
2026-01-28 16:18:00 +0100

    Update overall messaging pt.1

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -6,33 +6,28 @@ import LeavesCircleSvg from "@site/static/img/home/leaves-circle.svg";
 import JarPlantSvg from "@site/static/img/home/jar-plant.svg";
 import RootsPlantSvg from "@site/static/img/home/roots-plant.svg";
 import TwoPlantsSvg from "@site/static/img/home/two-plants.svg";
-import DaskSvg from "@site/static/img/home/dask.svg";
-import KeycloakSvg from "@site/static/img/home/keycloak.svg";
 import ScissorStringSvg from "@site/static/img/home/scissor-string.svg";
 import AwsSvg from "@site/static/img/home/aws.svg";
 import AzureSvg from "@site/static/img/home/azure.svg";
 import GcloudSvg from "@site/static/img/home/gcloud.svg";
-import CondaStoreSvg from "@site/static/img/home/conda-store.svg";
-import VSCodeSvg from "@site/static/img/home/vscode.svg";
-import ArgoSvg from "@site/static/img/home/argo.svg";
-import JupyterSvg from "@site/static/img/home/jupyter.svg";
 import styles from "./index.module.css";
 
 export default function HomePage() {
   return (
     <Layout
       title="Nebari Homepage"
-      description="Your open source data science platform. Built for scale, designed for collaboration."
+      description="TODO"
     >
       <main className={styles.main}>
         <section className={styles.heroSection}>
           <div className={styles.sectionContent}>
             <NebariLogoSvg width="158px" role="img" aria-label="Nebari logo" />
             <p className={styles.tagline1}>
-              Your open source data science & AI platform.
+              {/* Your open source data science & AI platform. */}
+              A fully open source stack for your AI.
             </p>
             <p className={styles.tagline2}>
-              Built for scale, designed for collaboration.
+              Core infrastructure, curated frameworks and apps, and everything in between.
             </p>
             <p>
               <a
@@ -48,65 +43,71 @@ export default function HomePage() {
 
         <section className={styles.pitchSection}>
           <div className={styles.sectionContent}>
-            <h2 className={styles.sectionHeading}>Why choose Nebari?</h2>
+            <h2 className={styles.sectionHeading}>Ecosystem of tools</h2>
             <ol className={styles.pitchList}>
               <li className={styles.pitchItem}>
                 <LeavesCircleSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>GitOps approach</span>
+                <span className={styles.pitchItemHeading}>Nebari classic</span>
                 <p>
-                  Integrated DevOps and security best practices for a robust
-                  deployment and better infrastructure management.
+                  Cloud-agnostic data science platform with environment management, role-based-access control,
+                  and curated app integrations for application deployment, scalability, monitoring, and more.
+                  <br/>
+                  <a href="TODO">Get started →</a>
                 </p>
               </li>
               <li className={styles.pitchItem}>
                 <JarPlantSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>Opinionated</span>
+                <span className={styles.pitchItemHeading}>Nebari core (early access)</span>
                 <p>
-                  Designed with integrations and configurations selected from
-                  real-world experience, so that you can use it out-of-the-box
-                  for a variety of data science workloads.
+                  New and robust core infrastructure layer for platform deployment on popular cloud providers.
+                  Security, authentication, role-based access, and observability built-in by design.
+                  <br/>
+                  <a href="TODO">Learn more →</a>
+                </p>
+              </li>
+              <li className={styles.pitchItem}>
+                <JarPlantSvg aria-hidden="true" height="150px" />
+                <span className={styles.pitchItemHeading}>Nebi (early access)</span>
+                <p>
+                  Tools to manage, share, and collaborate on AI software lifecycle.
+                  SBoMs, licensing, and compliance tools.
+                  <br/>
+                  <a href="TODO">Learn more →</a>
                 </p>
               </li>
               <li className={styles.pitchItem}>
                 <RootsPlantSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>
-                  Rooted in open source
-                </span>
+                <span className={styles.pitchItemHeading}>Usage-specific UIs (coming soon)</span>
                 <p>
-                  Developed with community in mind and under a BSD-3 license, we
-                  strive to contribute back to the upstream OSS projects
-                  wherever possible.
+                  Jupyter/VSCode for developers, chat & dashboard interfaces for AI users.
                 </p>
               </li>
               <li className={styles.pitchItem}>
                 <TwoPlantsSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>
-                  Collaboration-first
-                </span>
+                <span className={styles.pitchItemHeading}>AI model serving pack (coming soon)</span>
                 <p>
-                  Large teams can share work and iterate quickly with
-                  reproducible environments. Administrators can manage team
-                  resources effectively, all from the same platform.
+                  TODO
                 </p>
               </li>
               <li className={styles.pitchItem}>
-                <DaskSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>Dask powered</span>
+                <TwoPlantsSvg aria-hidden="true" height="150px" />
+                <span className={styles.pitchItemHeading}>Agentic AI pack (coming soon)</span>
                 <p>
-                  Nebari ships with Dask so you can scale your work to terabytes
-                  of data, leverage cloud instances with GPUs, and take
-                  advantage of adaptive scaling for managing costs.
+                  Frameworks for building, deploying, evaluating, and governing agent-based workflows.
+                </p>
+              </li>
+              <li className={styles.pitchItem}>
+                <TwoPlantsSvg aria-hidden="true" height="150px" />
+                <span className={styles.pitchItemHeading}>Data orchestration pack (coming soon)</span>
+                <p>
+                  Data engineering pipeline, governance of data and AI assets.
                 </p>
               </li>
               <li className={styles.pitchItem}>
                 <ScissorStringSvg aria-hidden="true" height="150px" />
-                <span className={styles.pitchItemHeading}>
-                  Your favorite tools
-                </span>
+                <span className={styles.pitchItemHeading}>Custom integrations</span>
                 <p>
-                  Built with open source infrastructure and tools to give you
-                  complete flexibility over your deployment and fit your
-                  team&apos;s specific needs.
+                  All Nebari tools are based on an open and extensible architecture, that allows quick integration of new tools you may need.
                 </p>
               </li>
             </ol>
@@ -159,13 +160,13 @@ export default function HomePage() {
 
         <section className={styles.getStartedSection}>
           <div className={styles.sectionContent}>
-            <h2 className={styles.sectionHeading}>Quick look at Nebari</h2>
+            <h2 className={styles.sectionHeading}>Quick look at Nebari classic</h2>
             <p>
               <a
                 href="/docs/get-started"
                 className={styles.getStartedLink2}
               >
-                Learn to deploy a Nebari instance&nbsp;&nbsp;
+                Learn to deploy a Nebari classic instance&nbsp;&nbsp;
                 <RightArrowSvg aria-hidden="true" />
               </a>
             </p>
@@ -186,36 +187,15 @@ export default function HomePage() {
 
         <section className={styles.integrationsSection}>
           <div className={styles.sectionContent}>
-            <h2 className={styles.sectionHeading}>Integrations</h2>
+            <h2 className={styles.sectionHeading}>Our Vision</h2>
             <p className={styles.integrationsSubheading}>
-              Nebari comes with out-of-the-box integrations to multiple tools in
-              the data science ecosystem.
+              Enable organizations to rapidly build with AI.
             </p>
             <ul className={styles.integrationsList}>
-              <li className={styles.integrationsItem}>
-                <CondaStoreSvg aria-hidden="true" />
-                conda-store
-              </li>
-              <li className={styles.integrationsItem}>
-                <VSCodeSvg aria-hidden="true" />
-                VSCode
-              </li>
-              <li className={styles.integrationsItem}>
-                <JupyterSvg aria-hidden="true" />
-                Jupyter
-              </li>
-              <li className={styles.integrationsItem}>
-                <KeycloakSvg aria-hidden="true" />
-                Keycloak
-              </li>
-              <li className={styles.integrationsItem}>
-                <DaskSvg aria-hidden="true" />
-                Dask
-              </li>
-              <li className={styles.integrationsItem}>
-                <ArgoSvg aria-hidden="true" />
-                Argo
-              </li>
+              <li className={styles.integrationsItem}>Ownership: Lorem ipsum</li>
+              <li className={styles.integrationsItem}>Open source: Lorem ipsum</li>
+              <li className={styles.integrationsItem}>Community-oriented development: Lorem ipsum</li>
+              <li className={styles.integrationsItem}>Modularity / expandability: Lorem ipsum</li>
             </ul>
           </div>
         </section>
