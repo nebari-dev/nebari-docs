6aabc01c71319e489b4e2bdd1aaf5b784ab0de43
andrewfulton9 <andrewfulton9@gmail.com>
2026-06-19 10:36:51 -0600

    fix linting errors

--- docs/src/theme/DocItem/Layout/index.tsx
+++ docs/src/theme/DocItem/Layout/index.tsx
@@ -18,13 +18,13 @@ export default function LayoutWrapper(props: Props): JSX.Element {
         <Admonition type="danger" title="Phase out notice">
           <p>
             Nebari is evolving. <strong>The original Nebari project is now called Nebari Classic, and is in maintenance mode.</strong>
-            <br></br>
-            <br></br>
-            We'll continue to provide security updates and key bug fixes; however, no new features are planned at this time.
-            We're using the lessons learned from Nebari Classic to build a new Nebari ecosystem of tools, on a more robust foundation with a modular architecture.
-            Nebari Classic's maintenance window will remain open until the new architecture reaches stability and feature parity, and current users are able to migrate, expected through the end of 2026.
-            <br></br>
-            <br></br>
+            <br />
+            <br />
+            We&apos;ll continue to provide security updates and key bug fixes; however, no new features are planned at this time.
+            We&apos;re using the lessons learned from Nebari Classic to build a new Nebari ecosystem of tools, on a more robust foundation with a modular architecture.
+            Nebari Classic&apos;s maintenance window will remain open until the new architecture reaches stability and feature parity, and current users are able to migrate, expected through the end of 2026.
+            <br />
+            <br />
             See the{' '}
             <Link to="/docs/introduction">current Nebari documentation</Link>{' '}
             for details on the new platform.
